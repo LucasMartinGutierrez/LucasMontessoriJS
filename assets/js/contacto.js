61d00470f238a32451fcc486f2dfb6b4d8dc3ee6
Lucas GUTIERREZ <lucag1@hotmail.com>
2022-07-11 20:30:27 -0300

    agergo evento tecla

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -1,3 +1,11 @@
+let input = document.getElementById("Productos");
+let primerlinea = document.getElementById("primerlinea");
+
+input.addEventListener("keydown", (e) => {
+    console.log(e.key);
+})
+
+
 let titulo = document.getElementById("titulo")
 console.log(titulo.innerText);
 titulo.innerText = "Hola, aceptamos tu sugerencia"
